b53068d77f838c8fbdabb833215f94095d35fd15
Nagashree-pai <mnagashreepai@gmail.com>
2025-02-07 19:49:51 +0530

    Code modification

--- CaeserCipher/code.py
+++ CaeserCipher/code.py
@@ -1,14 +1,28 @@
-def caesar_cipher(text, shift):
-    result = ""
+
+def encrypt(plain_text, key):
+    cipherText = ""
+    for char in plain_text:
+        if char.isalpha():
+            shift_base = ord('A') if char.isupper() else ord('a')
+            cipherText += chr((ord(char) - shift_base + key) % 26 + shift_base)
+        else:
+            cipherText += char
+    return cipherText
+
+# Decryption
+def decrypt(text, key):
+    pText = ""
     for char in text:
         if char.isalpha():
             shift_base = ord('A') if char.isupper() else ord('a')
-            result += chr((ord(char) - shift_base + shift) % 26 + shift_base)
+            pText += chr((ord(char) - shift_base - key) % 26 + shift_base)
         else:
-            result += char
-    return result
+            pText += char
+    return pText
 
-# Example usage
-text = "HELLO"
-shift = 3
-print("Encrypted:", caesar_cipher(text, shift))
+plain_text=input("Enter the text to encrypt:")
+key=int(input("Enter the key:"))
+print("Cipher text:",encrypt(plain_text,key))
+text=input("Enter the cipher text:")
+key=int(input("Enter the key:"))
+print("Plain text:",decrypt(text,key))
